8ed3aad7e1a3583e71880fee6359ef6222901c6a
Michał Starski <michalstarski99@gmail.com>
2019-09-06 20:34:03 +0200

    Added tests for the Sub model

--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,3 +1,5 @@
 class Sub < ApplicationRecord
   has_one_attached :background_pic
+
+  validates :name, presence: true
 end
--- test/models/sub_test.rb
+++ test/models/sub_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class SubTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @sub = Sub.new(name: 'foo')
+  end
+
+  test 'valid sub' do
+    assert @sub.valid?
+  end
+
+  test 'invalid without name' do
+    @sub.name = nil
+    refute @sub.valid?
+  end
 end
