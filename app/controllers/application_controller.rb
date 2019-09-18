d3007814561ec417cd459e8856d7c8e58bca71db
Michał Starski <michalstarski99@gmail.com>
2019-09-18 19:40:46 +0200

    complete authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    return User.find(session[:user_id]) unless session[:user_id].nil?
+  end
 end
