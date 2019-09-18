091af55de7c0c6ec7443f4021922d81391966044
Michał Starski <michalstarski99@gmail.com>
2019-09-18 13:27:05 +0200

    Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    email = params[:email]
+    password = params[:password]
+    user = User.find_by_email(email)
+    if user&.authenticate(password)
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in succesfully!'
+      redirect_to root_url
+    else
+      flash.now[:error] = 'Email or password is invalid'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out succesfully!'
+    redirect_to root_url
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resource :users 
-  
+  resource :users
+  resource :sessions, only: %i[new create destroy]
+
   root 'home#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
 end
