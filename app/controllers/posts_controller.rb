df8a9833c1ad7d331d1e8a74ffd4bdf341780036
Michał Starski <michalstarski99@gmail.com>
2019-09-29 10:26:59 +0200

    Checkpoint - Creating a Post"s new controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+
+  def create
+  end
+
+  def new
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, :subs
+  resources :users, :subs, :posts
   resource :sessions, only: %i[new create destroy]
 
   root 'home#index'
