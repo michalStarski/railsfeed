54269cd75293665118ce012f8e3f9e33f6f97c41
Michał Starski <michalstarski99@gmail.com>
2019-12-15 16:14:13 +0100

    Prepared data for creating my feed section on the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
     @highlighted_subs = Sub.order('RANDOM()').limit(5)
+
+    # Handle my feed
+    if current_user
+      @my_feed_posts = Post.where(
+        sub_id: current_user.subs
+      ).order('created_at DESC').limit(4).inspect
+    end
+
   end
 end
